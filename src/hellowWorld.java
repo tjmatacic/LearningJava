da7a3a3a9a3666a59713ff041916740fab52ec19
tyler <tjmatacic@student.ysu.edu>
2016-04-26 19:03:57 -0400

    first commit

---
+++ src/hellowWorld.java
@@ -0,0 +1,15 @@
+
+public class hellowWorld 
+{
+ public static void main (String[] args)
+ {
+ String [] greeting = new String[4];
+ greeting [0] = "welcome to core java";
+ greeting [1] = "by Cay";
+ greeting [2] = "and Gary";
+ greeting [3] = "and hellow world";
+ 
+		 for (int i=0; i<greeting.length; i++)
+			 System.out.println(greeting[i]);
+ }
+}
