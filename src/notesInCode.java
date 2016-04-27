905bdea2fc10ca20525ee3b8024d38abd5805f08
tyler <tjmatacic@student.ysu.edu>
2016-04-26 20:15:45 -0400

    added new class that I'll use to record my notes as I read

---
+++ src/notesInCode.java
@@ -0,0 +1,10 @@
+
+public class notesInCode {
+	public static void main(String[] args)
+	{
+		System.out.println("public: is called a access modifier, it  controls what other parts of the program can use this code "
+				+ "\n Class is a keyword and it's here to remind me that everything in a Java program lives in a class "
+				+ "\n Class is the building block of Java EVERYTHING in Java must be in a class");
+		/*System.out.println("Class is a keyword and it's here to remind me that everything in a Java program lives in a class, ");*/
+	}
+}
