fdf423445b17cf8f2a703e861ceb6dac32842565
tyler <tjmatacic@student.ysu.edu>
2016-04-27 12:47:50 -0400

    added more notes, fixed constant.java it works now!

--- src/constant.java
+++ src/constant.java
@@ -1,12 +1,12 @@
 
 public class constant {
-	public static void (String[] args)
+	public static void main(String[] args)
 	{
 		final double CM_PER_INCH = 2.54;
 		double paperWidth = 8.5;
 		double paperHeight = 11;
 		System.out.println ("Paper size in centemeter: "
-		+ paperWidth * CM_PER_INCH + " by ";
+		+ paperWidth * CM_PER_INCH + " by "
 		+ paperHeight * CM_PER_INCH);
 	}
 }
--- src/notesInCode.java
+++ src/notesInCode.java
@@ -4,7 +4,14 @@ public class notesInCode {
 	{
 		System.out.println("public: is called a access modifier, it  controls what other parts of the program can use this code "
 				+ "\n Class is a keyword and it's here to remind me that everything in a Java program lives in a class "
-				+ "\n Class is the building block of Java EVERYTHING in Java must be in a class");
+				+ "\n Class is the building block of Java EVERYTHING in Java must be in a class "
+				+ "\n every Java app must have a main method whose header is identical to the one used here"
+				+ "\n for function calls Java uses this syntax object.method(parameters)"
+				+ "\n if I want to \t\t tab a line I need a forward slash and a 't'"
+				+ "\ndeclerations and assignments, declerations are when you declare a variable type and name like: 'int salary' to assign the variable we need a = in this case initalizing salary might look like: 'salary= 10,000'"
+				+ "\n decleration: whenever you define a new variable WITH its type"
+				+ "\n Assignment: whenever you change the value of a variable by giving it a new value "
+				+ "\n Initialization: an assignment that is done TOGETHER with the decleration, or the first assignment that is done with a variable");
 		/*System.out.println("Class is a keyword and it's here to remind me that everything in a Java program lives in a class, ");*/
 	}
 }
