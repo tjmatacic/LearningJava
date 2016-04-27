113c81b1ac71168afcd209706f8b621cfcaa1423
tyler <tjmatacic@student.ysu.edu>
2016-04-26 21:22:29 -0400

    added constant class

---
+++ src/constant.java
@@ -0,0 +1,12 @@
+
+public class constant {
+	public static void (String[] args)
+	{
+		final double CM_PER_INCH = 2.54;
+		double paperWidth = 8.5;
+		double paperHeight = 11;
+		System.out.println ("Paper size in centemeter: "
+		+ paperWidth * CM_PER_INCH + " by ";
+		+ paperHeight * CM_PER_INCH);
+	}
+}
