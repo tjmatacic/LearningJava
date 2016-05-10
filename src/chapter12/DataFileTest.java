a3fc8b63cf8a4ba3b2c6f7ce9bb9bc81409a24af
tyler <tjmatacic@student.ysu.edu>
2016-05-10 11:59:11 -0400

    created ObjectFileTest class in chapter 12 package

--- src/chapter12/DataFileTest.java
+++ src/chapter12/DataFileTest.java
@@ -8,7 +8,7 @@ public class DataFileTest
 	{
 		Employee[] staff = new Employee[3];
 		
-		staff[0] = new Employee("CArl Cracker", 75000, 1987, 12, 15);
+		staff[0] = new Employee("Carl Cracker", 75000, 1987, 12, 15);
 		staff[1] = new Employee("Harry Hacker", 50000, 1989, 10, 1);
 		staff[2] = new Employee("Tony Tester", 40000, 1990, 3, 15);
 		
