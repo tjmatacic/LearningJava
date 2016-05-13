c064b5566ba9e36eebd02bb17336faae5fe2f5b7
tyler <tjmatacic@student.ysu.edu>
2016-05-13 12:18:01 -0400

    fixed ClassTester and getName classes

--- src/AdditionalPractice/getName.java
+++ src/AdditionalPractice/getName.java
@@ -4,24 +4,24 @@ public class getName
 {
 	private String friendName;
 	
-	public getName(String name)
-	{
-		friendName=name;
-	}
-	
 	public void setName(String name)
 	{
 		friendName = name;
 	}
 	
-	public String getName()
+	public getName(String name)
+	{
+		friendName=name;
+	}
+	
+	public String returnName()
 	{
 		return friendName;
 	}
 	
 	public void saying()
 	{
-		System.out.printf("My friend's name is %s\n", getName());
+		System.out.printf("My friend's name is %s\n", returnName());
 	}
 	
 }
