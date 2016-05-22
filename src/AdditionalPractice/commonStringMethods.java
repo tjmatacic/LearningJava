2bce8c356ef92d57aaf1f3e968316ba763c6cae6
tyler <tjmatacic@student.ysu.edu>
2016-05-22 10:50:20 -0400

    practiced indexing,concat, replace, and toUpperCase

--- src/AdditionalPractice/commonStringMethods.java
+++ src/AdditionalPractice/commonStringMethods.java
@@ -1,6 +1,7 @@
 package AdditionalPractice;
 
-public class commonStringMethods {
+public class commonStringMethods 
+{
 	
 	public static void main(String[] args)
 	{
@@ -19,6 +20,18 @@ public class commonStringMethods {
 			if(w.endsWith("r"))
 				System.out.println(w+ " ends with r");
 		}
+		
+		String s = "tylermatacictylermatacic";
+		System.out.println(s.indexOf('t'));//searched for the first index of 't'
+		System.out.println(s.indexOf("ty"));
+		
+		String a ="Bacon ";
+		String b ="monster";
+		
+		System.out.println(a.concat(b));
+		System.out.println(a.replace('B', 'F'));
+		System.out.println(b.toUpperCase());//useful in a database when you want everything to be uniform case
+		
 	}
 
 }
