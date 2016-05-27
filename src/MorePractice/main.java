1d35665feb3659c2e80693751fabaa207b368573
tyler <tjmatacic@student.ysu.edu>
2016-05-27 19:07:21 -0400

    more generic practice comparing different data types

--- src/MorePractice/main.java
+++ src/MorePractice/main.java
@@ -1,9 +1,10 @@
 package MorePractice;
+import java.util.*;
 
 public class main {
 	public static void main(String[] args)
 	{
-		Integer [] iray = {1,2,3,4};
+		/*Integer [] iray = {1,2,3,4};
 		Character [] cray = {'b','u','c','y'};
 		
 		printMe(iray);
@@ -17,6 +18,23 @@ public class main {
 		for (T b : x)
 			System.out.printf("%s ", b);
 		System.out.println();
+	}*/
+		System.out.println(max(23, 42, 1));
+		System.out.println(max("apples", "tots", "Chicken"));
+				
+	}
+	
+	public static <T extends Comparable<T>> T max (T a, T b, T c)
+	{
+		T m = a;
+		
+		if (b.compareTo(a) > 0)
+			m = b;
+		
+		if(c.compareTo(m) > 0)
+			m = c;
+		
+		return m;
 	}
 
 }
