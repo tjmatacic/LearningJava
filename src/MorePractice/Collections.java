06ab18d9aad7f5f60166a074a81302522bff73a2
tyler <tjmatacic@student.ysu.edu>
2016-05-29 14:50:21 -0400

    practicing collections

---
+++ src/MorePractice/Collections.java
@@ -0,0 +1,28 @@
+package MorePractice;
+import java.util.*;
+
+public class Collections {
+	public static void main(String[] args)
+	{
+		String[] things ={"eggs", "lasers", "hats", "pie"};
+		List<String> list1 = new ArrayList<String>();
+		
+		//add array items to list
+		for(String x: things)
+			list1.add(x);
+		
+		
+		String[] morethings = {"lasers", "hats"};
+		
+		List<String> list2 = new ArrayList<String>();
+		
+		//add array items to list
+		for(String y: morethings)
+			list2.add(y);
+		
+		for(int i = 0; i<list2.size(); i++){
+			System.out.printf("%s ",  list2.get(i));
+		}
+	}
+
+}
