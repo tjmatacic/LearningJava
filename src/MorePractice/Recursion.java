f9ca8e60d395b519a68c3a3309a6cafbc6ea1fc9
tyler <tjmatacic@student.ysu.edu>
2016-05-29 14:28:44 -0400

    practicing recursion

---
+++ src/MorePractice/Recursion.java
@@ -0,0 +1,20 @@
+package MorePractice;
+
+public class Recursion 
+{
+	public static void main(String[] args)
+	{
+		System.out.println(fact(5));
+	}
+	public static long fact(long n)
+	{
+	
+		if (n <=1)
+			return 1;
+		
+		else
+			return n * fact(n-1);
+	}
+}
+	
+
